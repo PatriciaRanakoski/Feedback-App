edc24a5dfcfbd106b42bc3fe0aefcc0ba99e78e2
Patricia Ranakoski <75952298+PatriciaRanakoski@users.noreply.github.com>
2022-02-12 14:36:45 +0000

    Add files via upload
    last update of my app

---
+++ src/src/components/FeedbackList.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import FeedBackItem from "./FeedBackItem"
+// import {motion, AnimatePresence} from 'frame-motion'
+
+
+function FeedbackList({ feedback, handleDelete }) {
+   if(!feedback || feedback.length === 0) {
+      return <p>No Feedbacks yet!!</p>
+   }
+
+  return (
+    <div className='feedback-list'>
+      {feedback.map((item) => (
+       <FeedBackItem key={item.id} item={item} 
+       handleDelete= {handleDelete}/>
+      ))}  
+    </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+}
+
+export default FeedbackList
